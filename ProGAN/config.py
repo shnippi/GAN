89388a51df4567478963f7be3bc52e151a9094b1
shnippi <janjosef.schnyder@bluewin.ch>
2021-06-12 12:59:40 +0200

    minor

--- ProGAN/config.py
+++ ProGAN/config.py
@@ -3,12 +3,12 @@ import torch
 from math import log2
 
 START_TRAIN_AT_IMG_SIZE = 128
-DATASET = 'celeb_dataset'
+DATASET = '../data/celeba_hq'
 CHECKPOINT_GEN = "generator.pth"
 CHECKPOINT_CRITIC = "critic.pth"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-SAVE_MODEL = True
-LOAD_MODEL = False
+SAVE_MODEL = False
+LOAD_MODEL = True
 LEARNING_RATE = 1e-3
 BATCH_SIZES = [32, 32, 32, 16, 16, 16, 16, 8, 4]
 CHANNELS_IMG = 3
